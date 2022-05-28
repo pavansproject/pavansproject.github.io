71be5027d5b23af45d0f08435f8eb4f2b40a0e6c
projectpavanapp <pavan936@yahoo.com>
2022-05-28 11:14:13 -0400

    messing with realm apps

--- something.js
+++ something.js
@@ -2,3 +2,8 @@ if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("https://pavansproject.github.io/sw.js");
 	console.log("Service worker is registered");
 }
+const id = tasktrackerithink-ytpzc;
+const config = {
+  id,
+};
+const app = new Realm.App(config);
