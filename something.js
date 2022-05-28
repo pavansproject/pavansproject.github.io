77f4b128a2407a715204531ff2135d399d1b4bdf
projectpavanapp <pavan936@yahoo.com>
2022-05-28 11:32:39 -0400

    Using Realm and Node to authenticate

--- something.js
+++ something.js
@@ -2,8 +2,10 @@ if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("https://pavansproject.github.io/sw.js");
 	console.log("Service worker is registered");
 }
-const id = tasktrackerithink-ytpzc;
-const config = {
-  id,
-};
-const app = new Realm.App(config);
+
+import * as Realm from "realm-web";
+const app = new Realm.App({id: application-1-ukdhb});
+
+const credentials = Realm.Credentials.anonymous();
+const user = await app.logIn(credentials);
+console.assert(user.id === app.currentUser.id);
