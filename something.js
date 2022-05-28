aa0b7b2453c2179140db81ffd11c9383dbebd3b6
projectpavanapp <pavan936@yahoo.com>
2022-05-28 11:43:55 -0400

    the other tutorial I follow

--- something.js
+++ something.js
@@ -4,8 +4,13 @@ if ("serviceWorker" in navigator) {
 }
 
 //import * as Realm from "realm-web";
-const app = new Realm.App({id: application-1-ukdhb});
+/*const app = new Realm.App({id: application-1-ukdhb});
 
 const credentials = Realm.Credentials.anonymous();
 const user = await app.logIn(credentials);
-console.assert(user.id === app.currentUser.id);
+console.assert(user.id === app.currentUser.id);*/
+const id = application-1-ukdhb;
+const config = {
+	id,
+};
+const app = new Realm.App(config);
