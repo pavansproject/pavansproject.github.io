e7cbe706512b615568c7d2b0ace6ae623c0c003d
projectpavanapp <pavan936@yahoo.com>
2022-06-30 14:55:40 -0400

    this should magically make it work

--- confirmEmail.js
+++ confirmEmail.js
@@ -1,8 +1,29 @@
+// const id = "application-1-ukdhb"; 
+// const config = {
+// 	id,
+// };
+// const app = Realm.App.getApp("application-1-ukdhb");
+
+
+
+
+
 const id = "application-1-ukdhb"; 
 const config = {
 	id,
 };
+
 const app = Realm.App.getApp("application-1-ukdhb");
+
+const {
+	BSON: {ObjectID},
+} = Realm;
+
+
+
+
+
+
 
 const string = window.location.search;
 const urlstuff = new URLSearchParams(string);
