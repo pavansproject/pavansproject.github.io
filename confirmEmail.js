a749b573d19d5c45c8dd0ebd80781deabf9a2a06
projectpavanapp <pavan936@yahoo.com>
2022-06-30 14:44:16 -0400

    ok what about this

--- confirmEmail.js
+++ confirmEmail.js
@@ -7,4 +7,6 @@ const app = Realm.App.getApp("application-1-ukdhb");
 const string = window.location.search;
 const urlstuff = new URLSearchParams(string);
 
-
+async function hi() {
+    const hello = await confirmUser()
+}
